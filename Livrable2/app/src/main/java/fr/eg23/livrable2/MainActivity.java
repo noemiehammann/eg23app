ff53c78fbc926139dbaebae3121ed78e6dca2f61
noemie.hammann <noemie.hammann@utt.fr>
2021-05-30 16:35:13 +0200

    début de menu

--- Livrable2/app/src/main/java/fr/eg23/livrable2/MainActivity.java
+++ Livrable2/app/src/main/java/fr/eg23/livrable2/MainActivity.java
@@ -3,6 +3,7 @@ package fr.eg23.livrable2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,33 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Menu : liaison bouton de menu et autres pages
+
+        // Menu 1
+        final Button menu1= (Button) findViewById(R.id.menu1);
+        // Création d’un évènement qui attend un clic sur le bouton
+        menu1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) { // fonction déclenchée sur le clic du bouton
+                // Création d’une activité associée à l’exécution de MaGestionListe.class
+                Intent intent = new Intent(MainActivity.this, GestionDesEnseignants.class);
+                // Exécution de l’activité : ouverture de la fenêtre
+                startActivity(intent);
+            }
+        });
+
+        // Menu 2
+        final Button menu1= (Button) findViewById(R.id.menu1);
+        // Création d’un évènement qui attend un clic sur le bouton
+        menu1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) { // fonction déclenchée sur le clic du bouton
+                // Création d’une activité associée à l’exécution de MaGestionListe.class
+                Intent intent = new Intent(MainActivity.this, GestionDesEnseignants.class);
+                // Exécution de l’activité : ouverture de la fenêtre
+                startActivity(intent);
+            }
+        });
     }
 }
